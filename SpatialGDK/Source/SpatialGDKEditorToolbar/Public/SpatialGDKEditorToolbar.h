83085c7062ea375f7339a8f84a19103c2446d458
Sébastien Rombauts (Darewise) <sebastien@darewise.com>
2019-02-24 22:18:37 +0100

    Add the Local Workflow into a combo menu at the end of the GDK Toolbar
    - cook a map
    - launch a dedicated server
    - launch a networked client
    
    TODOs for the server with rendering not working

--- SpatialGDK/Source/SpatialGDKEditorToolbar/Public/SpatialGDKEditorToolbar.h
+++ SpatialGDK/Source/SpatialGDKEditorToolbar/Public/SpatialGDKEditorToolbar.h
@@ -13,6 +13,7 @@ class FToolBarBuilder;
 class FMenuBuilder;
 class FUICommandList;
 class USoundBase;
+class FMonitoredProcess;
 class FSpatialGDKEditor;
 
 DECLARE_LOG_CATEGORY_EXTERN(LogSpatialGDKEditorToolbar, Log, All);
@@ -43,6 +44,7 @@ private:
 	void SetupToolbar(TSharedPtr<FUICommandList> PluginCommands);
 	void AddToolbarExtension(FToolBarBuilder& Builder);
 	void AddMenuExtension(FMenuBuilder& Builder);
+	TSharedRef<SWidget> GenerateComboMenu();
 
 	void StartSpatialOSButtonClicked();
 	void StopSpatialOSButtonClicked();
@@ -53,6 +55,24 @@ private:
 	void CreateSnapshotButtonClicked();
 	void SchemaGenerateButtonClicked();
 	void OnPropertyChanged(UObject* ObjectBeingModified, FPropertyChangedEvent& PropertyChangedEvent);
+
+	void LoadSublevels();
+	bool CanLoadSublevels() const;
+	FText LoadSublevelsTooltip() const;
+	void CookMap();
+	bool CanCookMap() const;
+	FText CookMapTooltip() const;
+	void LaunchDedicatedServer();
+	bool CanLaunchDedicatedServer() const;
+	FText LaunchDedicatedServerTooltip() const;
+	void ToggleServerWithRendering();
+	bool IsServerWithRendering() const;
+	void LaunchNetworkedClient();
+	bool CanLaunchNetworkedClient() const;
+	FText LaunchNetworkedClientTooltip() const;
+	void PackageNetworkedClient();
+	bool CanPackageNetworkedClient() const;
+	FText PackageNetworkedClientTooltip() const;
 
 private:
 	bool CanExecuteSchemaGenerator() const;
@@ -73,9 +93,14 @@ private:
 	FProcHandle SpatialOSStackProcHandle;
 	bool bStopSpatialOnExit;
 	
-	uint32 SpatialOSStackProcessID;
-
 	TWeakPtr<SNotificationItem> TaskNotificationPtr;
+
+	// CORVUS_BEGIN Local Workflow
+	bool bServerWithRendering = false;
+	FProcHandle ServerProcessHandle;
+	TSharedPtr<FMonitoredProcess> CookMapProcess;
+	TSharedPtr<FMonitoredProcess> PackageClientProcess;
+	// CORVUS_END
 
 	// Sounds used for execution of tasks.
 	USoundBase* ExecutionStartSound;
