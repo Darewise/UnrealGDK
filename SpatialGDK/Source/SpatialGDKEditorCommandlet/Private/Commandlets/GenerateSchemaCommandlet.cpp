3d68c3cb9581ab96b80a0c70f76a28e013876f82
Sébastien Rombauts (Darewise) <sebastien@darewise.com>
2019-07-02 14:48:46 +0200

    GenerateSchema Commandlet now generate schema only for one Map to avoid loading all assets
    Warning: This means that we never do a Full Scan Schema generation on the build machine,
    and that we will need to load explicitly the game mode class (see next commit).
    
    1. This is strictly needed to avoid failures on trying to load deprecated assets
    2. This also reduce a lot the time to generate schema

--- SpatialGDK/Source/SpatialGDKEditorCommandlet/Private/Commandlets/GenerateSchemaCommandlet.cpp
+++ SpatialGDK/Source/SpatialGDKEditorCommandlet/Private/Commandlets/GenerateSchemaCommandlet.cpp
@@ -6,6 +6,11 @@
 #include "SpatialGDKEditorCommandletPrivate.h"
 #include "SpatialGDKEditorSchemaGenerator.h"
 
+#include "Engine/ObjectLibrary.h"
+#include "Engine/World.h"
+#include "FileHelpers.h"
+#include "Misc/Paths.h"
+
 using namespace SpatialGDKEditor::Schema;
 
 UGenerateSchemaCommandlet::UGenerateSchemaCommandlet()
@@ -40,7 +45,7 @@ int32 UGenerateSchemaCommandlet::Main(const FString& Args)
 	TArray<FString> Tokens;
 	TArray<FString> Switches;
 	TMap<FString, FString> Params;
-	ParseCommandLine(*Args, Tokens, Switches);
+	ParseCommandLine(*Args, Tokens, Switches, Params);
 
 	if (!HandleOptions(Switches))
 	{
@@ -48,10 +53,33 @@ int32 UGenerateSchemaCommandlet::Main(const FString& Args)
 		return 1;
 	}
 
+	// CORVUS_BEGIN
+	const FString* MapName = Params.Find(TEXT("Map"));
+	if (MapName)
+	{
+		const FString MapDir = TEXT("/Game");
+		const TArray<FString> MapFilePaths = GetAllMapPaths(MapDir);
+		for (FString MapFilePath : MapFilePaths)
+		{
+			if (MapFilePath.EndsWith(*MapName))
+			{
+				// Load the World
+				UE_LOG(LogSpatialGDKEditorCommandlet, Display, TEXT("LoadMap %s"), *MapFilePath);
+				if (!FEditorFileUtils::LoadMap(MapFilePath))
+				{
+					UE_LOG(LogSpatialGDKEditorCommandlet, Display, TEXT("Failed to load map %s"), *MapFilePath);
+				}
+				break;
+			}
+		}
+	}
+	const bool bFullScan = (MapName == nullptr);
+	// CORVUS_END
+
 	//Generate Schema!
 	bool bSchemaGenSuccess;
 	FSpatialGDKEditor SpatialGDKEditor;
-	if (SpatialGDKEditor.GenerateSchema(true))
+	if (SpatialGDKEditor.GenerateSchema(bFullScan)) // CORVUS
 	{
 		UE_LOG(LogSpatialGDKEditorCommandlet, Display, TEXT("Schema Generation Completed!"));
 		bSchemaGenSuccess = true;
@@ -66,3 +94,24 @@ int32 UGenerateSchemaCommandlet::Main(const FString& Args)
 
 	return bSchemaGenSuccess ? 0 : 1;
 }
+
+// CORVUS_BEGIN
+TArray<FString> UGenerateSchemaCommandlet::GetAllMapPaths(FString InMapsPath)
+{
+	UObjectLibrary* ObjectLibrary = UObjectLibrary::CreateLibrary(UWorld::StaticClass(), false, true);
+	ObjectLibrary->LoadAssetDataFromPath(InMapsPath);
+	TArray<FAssetData> AssetDatas;
+	ObjectLibrary->GetAssetDataList(AssetDatas);
+	UE_LOG(LogSpatialGDKEditorCommandlet, Display, TEXT("Found %d maps:"), AssetDatas.Num());
+
+	TArray<FString> Paths = TArray<FString>();
+	for (FAssetData& AssetData : AssetDatas)
+	{
+		FString Path = AssetData.PackageName.ToString();
+		Paths.Add(Path);
+		UE_LOG(LogSpatialGDKEditorCommandlet, Log, TEXT("\t%s"), *Path);
+	}
+
+	return Paths;
+}
+// CORVUS_END
